326510c29474a58ddad946aa620dbd6f16e4dd11
Zach Martis <zachpmartis@gmail.com>
2022-11-24 00:17:10 -0500

    started part 2

---
+++ 2020/16_Ticket_Translation/part_2.ts
@@ -0,0 +1,86 @@
+import { readFileSync } from 'fs'
+import {
+  camelCase,
+  each,
+  every,
+  filter,
+  flatten,
+  intersection,
+  map,
+  some,
+  split,
+  toNumber,
+} from 'lodash'
+
+interface Rules {
+  [key: string]: RangeObject
+}
+
+interface RangeObject {
+  lowMin: number
+  lowMax: number
+  highMin: number
+  highMax: number
+}
+
+const input = split(readFileSync('input.txt', 'utf-8'), '\n\n')
+
+let rules: Rules = {}
+each(split(input[0], '\n'), (line) => {
+  const splitLine = split(line, ': ')
+  const ranges = split(splitLine[1], ' or ')
+  const lowRange = split(ranges[0], '-')
+  const highRange = split(ranges[1], '-')
+  rules[camelCase(splitLine[0])] = {
+    lowMin: toNumber(lowRange[0]),
+    lowMax: toNumber(lowRange[1]),
+    highMin: toNumber(highRange[0]),
+    highMax: toNumber(highRange[1]),
+  }
+})
+const myTicket = split(split(input[1], ':\n')[1], ',')
+const nearbyTickets = map(split(split(input[2], ':\n')[1], '\n'), (line) =>
+  map(split(line, ','), (string) => toNumber(string))
+)
+
+// ---------------------------
+
+const invalidNumberSet = new Set<number>()
+
+function run() {
+  const validNearbyTickets = filter(nearbyTickets, (ticket) => {
+    if (intersection(ticket, [...invalidNumberSet]).length > 0) {
+      return false
+    } else if (ticketIsInvalid(ticket)) {
+      return false
+    } else {
+      return true
+    }
+  })
+}
+
+function ticketIsInvalid(ticket: number[]) {
+  return some(
+    map(ticket, (number) => {
+      if (numberIsInvalid(number)) {
+        invalidNumberSet.add(number)
+        return true
+      }
+      return false
+    })
+  )
+}
+
+function numberIsInvalid(number: number) {
+  const keys = Object.keys(rules)
+  return every(
+    map(
+      keys,
+      (key) =>
+        !(number >= rules[key].lowMin && number <= rules[key].lowMax) &&
+        !(number >= rules[key].highMin && number <= rules[key].highMax)
+    )
+  )
+}
+
+run()
