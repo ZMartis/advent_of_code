26c4005bb9f8932181525e82ce6a39d0cef7e468
Zach Martis <zachpmartis@gmail.com>
2022-11-25 23:24:29 -0500

    completed part 2

---
+++ 2020/1_Report_Repair/part_2.ts
@@ -0,0 +1,20 @@
+import { readFileSync } from 'fs'
+import { filter, includes, map, split, toNumber } from 'lodash'
+
+const input = map(split(readFileSync('input.txt', 'utf-8'), '\n'), (string) =>
+  toNumber(string)
+)
+
+let numberFound = false
+for (const mainNumber of input) {
+  if (numberFound === true) {
+    break
+  }
+  for (const number of input) {
+    if (includes(input, 2020 - mainNumber - number)) {
+      console.log(mainNumber * number * (2020 - mainNumber - number))
+      numberFound = true
+      break
+    }
+  }
+}
