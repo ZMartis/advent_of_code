8ead2f3141addc3f0271d11e5929a921c33911fc
Zach Martis <zachpmartis@gmail.com>
2022-11-27 08:11:52 -0500

    completed part 2

--- 2020/8_Handheld_Halting/part_2.ts
+++ 2020/8_Handheld_Halting/part_2.ts
@@ -1,44 +1,56 @@
 import { readFileSync } from 'fs'
-import { includes, split, toNumber } from 'lodash'
+import { cloneDeep, each, filter, includes, map, split, toNumber } from 'lodash'
 
-const input = split(readFileSync('input.txt', 'utf-8'), '\n')
+interface CommandLine {
+  command: 'jmp' | 'acc' | 'nop'
+  value: number
+}
 
-const indexesRun: number[] = []
+const input: CommandLine[] = map(
+  split(readFileSync('input.txt', 'utf-8'), '\n'),
+  (line) => {
+    const splitLine = split(line, ' ')
+    return {
+      command: splitLine[0] as CommandLine['command'],
+      value: toNumber(splitLine[1]),
+    }
+  }
+)
 
-let accumulator = 0
-let currentIndex = 0
+// ------------------------
 
-function run(input) {
-  while (!includes(indexesRun, currentIndex) && currentIndex < input.length) {
-    const command = split(input[currentIndex], ' ')[0]
-    const number = toNumber(split(input[currentIndex], ' ')[1])
+const finalList = map(input, (line, index) => {
+  const coppiedInput = cloneDeep(input)
+  coppiedInput[index].command =
+    line.command === 'acc' ? 'acc' : line.command === 'jmp' ? 'nop' : 'jmp'
+  return runProgram(coppiedInput)
+})
 
-    if (command === 'acc') {
-      accumulator += number
+function runProgram(commandLines: CommandLine[]) {
+  const indexesRun: number[] = []
+
+  let currentIndex = 0
+  let accumultaor = 0
+
+  while (
+    !includes(indexesRun, currentIndex) &&
+    currentIndex < commandLines.length
+  ) {
+    if (commandLines[currentIndex].command === 'acc') {
+      accumultaor += commandLines[currentIndex].value
       indexesRun.push(currentIndex)
       currentIndex++
-    } else if (command === 'jmp') {
-      checkReverse()
+    } else if (commandLines[currentIndex].command === 'jmp') {
       indexesRun.push(currentIndex)
-      currentIndex += number
-    } else if (command === 'nop') {
-      checkReverse()
+      currentIndex += commandLines[currentIndex].value
+    } else if (commandLines[currentIndex].command === 'nop') {
       indexesRun.push(currentIndex)
       currentIndex++
     }
   }
+
+  // return accumultaor
+  return currentIndex >= commandLines.length - 1 ? accumultaor : undefined
 }
 
-function checkReverse() {
-  const inputCopy = input
-  let command = split(inputCopy[currentIndex], ' ')[0]
-  let number = toNumber(split(inputCopy[currentIndex], ' ')[1])
-
-  if (command === 'jmp') {
-    inputCopy[currentIndex] === `nop ${number}`
-  } else {
-    inputCopy[currentIndex] === `jmp ${number}`
-  }
-}
-
-console.log(accumulator)
+console.log(filter(finalList, (answer) => answer !== undefined)[0])
